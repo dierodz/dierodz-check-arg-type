cc4cec447d327ff68078150009fd732afdcd2c04
Diego Rodriguez <dierodz91@gmail.com>
2020-08-18 16:09:10 -0300

    Add check type object

--- src/ArgTypes/index.js
+++ src/ArgTypes/index.js
@@ -3,6 +3,7 @@ const PrimitiveType = require('../PrimitiveType')
 const ArgTypes = {
   boolean: PrimitiveType.bind(null, 'boolean'),
   number: PrimitiveType.bind(null, 'number'),
+  object: PrimitiveType.bind(null, 'object'),
   string: PrimitiveType.bind(null, 'string')
 }
 
